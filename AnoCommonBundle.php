5b0a30bf059a732032030d7ccdc434fb75d8461a
Benjamin Dulau <benjamin.dulau@anonymation.com>
2012-07-01 11:45:10 +0200

    First commit

---
+++ AnoCommonBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * This file is part of the AnoCommonBundle
+ *
+ * (c) anonymation <contact@anonymation.com>
+ *
+ */
+
+namespace Ano\Bundle\CommonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AnoCommonBundle extends Bundle
+{
+}
